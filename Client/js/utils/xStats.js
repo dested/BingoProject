a8dd2f0f5f666200eb6a2a71f07e07813d7d62b2
Salvatore <dested@gmail.com>
2014-02-02 14:56:15 -0700

    added stats

---
+++ Client/js/utils/xStats.js
@@ -0,0 +1,16 @@
+define(['../libs/stats.min'],function(){
+    var stats = new Stats();
+    stats.setMode(0); // 0: fps, 1: ms
+
+// Align top-left
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.right = '0px';
+    stats.domElement.style.top = '0px';
+
+    document.body.appendChild( stats.domElement );
+
+    setInterval( function () {
+        stats.begin();
+        stats.end();
+    }, 1000 / 60 );
+})
